39f3e5a7e31cf4447f76f9a123098bc4718df5ec
GabrielInacio03 <inaciogs03@gmail.com>
2024-02-19 09:40:24 -0300

    criando modelo de domínio

---
+++ LanchesMac/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public long CategoriaId { get; set; } //chave primária
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+        public List<Lanche> Lanches { get; set;}
+    }
+}
