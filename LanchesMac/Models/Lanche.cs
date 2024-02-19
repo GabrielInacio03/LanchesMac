c3835adb3f936c1fe55d64264cb193ab85f377de
GabrielInacio03 <inaciogs03@gmail.com>
2024-02-19 14:48:10 -0300

    Exemplo de Data Annotations

--- LanchesMac/Models/Lanche.cs
+++ LanchesMac/Models/Lanche.cs
@@ -1,8 +1,18 @@
-﻿namespace LanchesMac.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LanchesMac.Models
 {
+    [Table("Lanches")]
     public class Lanche
     {
+        [Key]
         public int LancheId { get; set; }
+        
+        [Required(ErrorMessage = "O nome do lanche deve ser informado")]
+        [Display(Name = "Nome do Lanche")]
+        [MinLength(10, ErrorMessage = "Nome deve ter no mínimo {1} caracteres")]
+        [MaxLength(60, ErrorMessage = "NOme deve ter no máximo {1} caracteres")]
         public string Nome { get; set; }
         public string DescricaoCurta { get; set; }
         public string DescricaoDetalhada { get; set; }
