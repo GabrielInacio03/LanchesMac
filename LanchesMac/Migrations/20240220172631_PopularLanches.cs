e96e7913ca030002d39ce5981236b3f98127d17d
GabrielInacio03 <inaciogs03@gmail.com>
2024-02-20 14:44:07 -0300

    Populando Lanches

---
+++ LanchesMac/Migrations/20240220172631_PopularLanches.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class PopularLanches : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Lanches(Nome,DescricaoCurta, DescricaoDetalhada, Preco, ImagemUrl, ImagemThumbnailUrl," +
+                "IsLanchePreferido,EmEstoque, CategoriaId) VALUES" +
+                "('Brutão', 'Especial da nossa lanchonete', 'pão, alface, tomate,hamburger, queijo grelhado e ovo.', 19.99, NULL, NULL," +
+                "1, 1, 1)");
+
+            migrationBuilder.Sql("INSERT INTO Lanches(Nome,DescricaoCurta, DescricaoDetalhada, Preco, ImagemUrl, ImagemThumbnailUrl," +
+                "IsLanchePreferido,EmEstoque, CategoriaId) VALUES" +
+                "('Hot Dog', 'Hot Dog básico', 'pão e salsicha.', 9.99, NULL, NULL," +
+                "0, 1, 1)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM Lanches");
+        }
+    }
+}
