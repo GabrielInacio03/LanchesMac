95600e37280f92876533e77d4b9ed77aee1bc3dd
GabrielInacio03 <inaciogs03@gmail.com>
2024-02-20 21:53:25 -0300

    Repositorio categorias

---
+++ LanchesMac/Repositories/CategoriaRepository.cs
@@ -0,0 +1,19 @@
+﻿using LanchesMac.Context;
+using LanchesMac.Models;
+using LanchesMac.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    {
+        private readonly AppDbContext _context;
+
+        public CategoriaRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Categoria> Categorias => _context.Categorias;//throw new NotImplementedException();
+    }
+}
