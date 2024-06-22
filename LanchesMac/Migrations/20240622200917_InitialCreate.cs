c86a260410fad2979ecf82d70535db693015df2b
Gabriel da Silva Inácio <Inaciogs03@gmail.com>
2024-06-22 17:14:29 -0300

    [LanchesMac] - migracao

---
+++ LanchesMac/Migrations/20240622200917_InitialCreate.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LanchesMac.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Lanche",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Descricao = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Preco = table.Column<double>(type: "float", nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Lanche", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Lanche");
+        }
+    }
+}
---
+++ LanchesMac/Models/Utils/LancheUtils.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LanchesMac.Models.Utils
+{
+    public static class LancheUtils
+    {
+        public static bool IsValidId(long id)
+        {
+            if (id == null)
+                return false;
+            else
+                return true;
+            
+        }
+    }
+}
